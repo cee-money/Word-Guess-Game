4c78a59b9350e74966a5512fce3f7bf650e9407b
cmoney <moneymaker.carolyn@gmail.com>
2019-06-02 21:38:36 -0600

    created counters, defined variables, created arrays and started function with necessary if/else statements

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,47 +1,56 @@
-//directions
-
-//wins, losses, guesses counters
+// Wins and guesses counters
 var wins = 0;
 var guesses = 12;
 
-// computer chooses word from an array
-var computerChoice = ["Denali", "Rainier", "Everest", "Pikes", "Kilimanjaro", "Hood", "Longs", "Aoraki", "Shasta", "Crestone", "Harvard"];
+// An array of choices from which the computer will pick a work
+var computerChoice = ["denali", "rainier", "everest", "pikes", "hood", "longs", "aoraki", "Harvard"];
+
+// Array of wrong user guesses
+var wrongGuessesArray = [];
+
+// How the computer will randomly choose the word
+var word = computerChoice[Math.floor(Math.random() * computerChoice.length)];
+
+// How dashses will be displayed to show the user the length of the random word the computer chose
+var answerArray = [];
+for (var i = 0; i < word.length; i++) {
+    answerArray[i] = "_";
+}
 
 // Variables to hold references to all html sections game will update
 var winsText = document.getElementById("wins");
 var userGuessText = document.getElementById("userGuess");
 var guessesText = document.getElementById("guesses");
-var guessesHistoryText = document.getElementById("guessesHistory");
+var guessHistoryText = document.getElementById("guessHistory");
+var answerArrayText = document.getElementById("word");
 
-// userGuess is shown
-//     can only guess each letter once
-// when wins game resets
-// when user wins, image of word displays in the top left column
-
-// Computer chooses word randomly from computerChoices array
-var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+// Display dashes for random word computer chose
+answerArrayText.textContent = answerArray.join(" ");
 
 // User keystroke to trigger a win or loss and -1 guesses left until guesses left = 0
 document.onkeyup = function(event) {
     var userGuess = event.key;
-// if my guess === a letter in the computer's word, add letter to current word display game reset (set counters to 0 0 9, display original HTML, computer to make new guess)
-    if (userGuess === computerGuess) { 
-        win++;
-        guesses=12;
-        winsText.textContent = "Wins: " + wins;
-        guessesText.textContent =  "Number of Guesses Remaining: " + guesses;
-        guessesHistory.textContent= "Letters Already Guessed: ";
-        var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
- // else if my guess !== computer's guess, I get 8 more guesses then game resets upon win or loss (set counters to 0 0 9, display original HTML)
-    } else if (guesses > 1) {
+
+    if (word.includes(userGuess)) {
+        var index = word.indexOf(userGuess);
         guesses--;
-        guessesText.textContent =  "Number of Guesses Remaining: " + guesses;
+        //if: 
+        //else if:
+    } else if (!word.includes(userGuess)) {
         guessesHistoryText.append(" " + userGuess + " ");
-// else my guess !=== computer's guess and I have no more guesses, so loss counter increments by 1 and game resets
-    } else {
-        guesses=12;
-        guessesText.textContent =  "Number of Guesses Remaining: " + guesses;
-        guessesHistory.textContent = "Letters Already Guessed: ";
-        var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-    }
+        guesses--;
+        //if: 
+        //else if:
+    } 
+
 }
+
+
+//Steps:
+// 1. Pick a random word.
+// 2. Take the player’s guess.
+// 3. Check that the player’s guess is a valid letter.
+// 4. Keep track of letters the player has guessed.
+// 5. Show the player their progress.
+// 6. Finish when the player has guessed the word.
+   
